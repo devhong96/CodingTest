2f439e436accad092fb2e4b76acd150b3ba339ae
Seho0218 <ghdtpgh8913@gmail.com>
2022-12-14 02:36:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int ans = scan.next().charAt(0);
+        
+        System.out.println(ans);
+    }
+}
+
