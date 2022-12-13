edf95a99e30f2f7d1ec23c58c36d6153428a8f96
Seho0218 <ghdtpgh8913@gmail.com>
2022-12-14 01:10:23 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        int year=scan.nextInt();
+        
+        if(year%4==0){
+           if(year%100!=0 || year%400==0){
+               System.out.print("1");    
+           }else{
+               System.out.print("0");}
+        }else{
+            System.out.print("0");
+        }
+    }
+}
