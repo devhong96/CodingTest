55d4732a2bde65763fe0a6b31f99bc8f40ba3eb6
Seho0218 <ghdtpgh8913@gmail.com>
2022-12-14 02:03:20 +0900

    [Bronze II] Title: 상수, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+
+        A= Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+        B= Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+
+        System.out.print(A > B ? A : B);
+
+    }
+}
+
