49fd5649170ecd93580600cf9f04f05ebfc96425
Seho0218 <ghdtpgh8913@gmail.com>
2022-12-14 02:37:36 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        
+        String a=scan.nextLine();
+        System.out.println(a+"??!");
+    }
+}
