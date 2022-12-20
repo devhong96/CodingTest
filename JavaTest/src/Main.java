f068f1a12f965d352075868d4a53fd68d12b78a9
Seho0218 <ghdtpgh8913@gmail.com>
2022-12-20 22:20:57 +0900

    백준 1978

--- JavaTest/src/Main.java
+++ JavaTest/src/Main.java
@@ -1,9 +1,25 @@
-import java.util.Scanner;
+import java.io.*;
 
-class Main {
-    public static void main(String[] args){
+class Main{
+    public static void main(String[] args) throws IOException{
 
-        Scanner scan=new Scanner(System.in);
-        scan.close();
-    }       
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+        int[] arr = new int[count];
+        int answer=0;
+
+        for(int i=0; i < count; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+
+            for(int k=2; k<=arr[i]; i++){
+                if(arr[i]%k==0){
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+
+    }
 }
