a56a16339f248d5ebb96787209b80629b68fd7c8
Seho0218 <ghdtpgh8913@gmail.com>
2022-12-30 05:50:18 +0900

    Update and rename 콜라츠 추.java to 콜라츠 추측.java

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -18,4 +18,8 @@ class Solution {
         } while (num!=1);
         return count;
     }
-}
+//        for (int i = 0; i < 500; i++) {
+//            if(num==1) return i;
+//            num = (num%2==0) ? num/2 : num*3+1;
+//        }
+//        return -1;
