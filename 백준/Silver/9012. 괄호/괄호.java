5429344edf41b8d4d720ccf5c0da759973127615
Seho0218 <ghdtpgh8913@gmail.com>
2023-03-01 03:46:49 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < num; i++) {
+            String answer = "YES";
+            String str = br.readLine();
+
+            for(char bracket : str.toCharArray()){
+                if(bracket=='(') stack.push(bracket);
+                else if(stack.empty()){
+                    answer = "NO";
+                    break;
+                }
+                else stack.pop();
+            }
+            if(!stack.empty()) answer = "NO";
+            System.out.println(answer);
+            stack.clear();
+        }
+    }
+}
+
