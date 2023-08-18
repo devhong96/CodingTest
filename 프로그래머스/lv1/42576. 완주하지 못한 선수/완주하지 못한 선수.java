4ed8d9fa13233a348472200144fba2c7cd5aedd1
Seho0218 <dev.hsayho@gmail.com>
2023-08-19 02:39:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.12 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String name : participant) {
+            map.putIfAbsent(name, 0);
+            map.put(name, map.get(name) + 1);
+        }
+
+        for (String name : completion) {
+            int value = map.get(name) - 1;
+            map.put(name, value);
+            if(value == 0) map.remove(name);
+        }
+        return map.keySet().iterator().next();
+    }
+}
