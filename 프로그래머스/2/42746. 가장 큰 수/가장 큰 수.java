79dc8b32ef25df5876769193f832750bba33a4c6
Seho0218 <dev.hsayho@gmail.com>
2024-01-06 13:35:57 +0900

    [level 2] Title: 가장 큰 수, Time: 13.71 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(int[] numbers) {
+        return Arrays.stream(numbers)
+            .mapToObj(String::valueOf)
+            .sorted((s1, s2) -> {
+    
+                return (s2+s1).compareTo(s1+s2);
+            })
+            .collect(Collectors.joining(""))
+            .replaceAll("^0+","0");
+    }
+}
