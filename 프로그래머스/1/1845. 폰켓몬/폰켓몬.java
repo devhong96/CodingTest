38e475c62f7a4a3f68b42ab429f53c3152b4157d
Seho0218 <dev.hsayho@gmail.com>
2024-01-07 13:46:12 +0900

    [level 1] Title: 폰켓몬, Time: 2.19 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int pick = nums.length/2;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums)set.add(num);
+        if(set.size() >= pick) answer = pick;
+        if(set.size() < pick) answer = set.size();
+        return answer;
+    }
+}
