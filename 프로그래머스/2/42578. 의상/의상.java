3757ecd067dbd5f71c78813bf784cd4a48d47887
Seho0218 <dev.hsayho@gmail.com>
2024-01-11 16:20:14 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String[] s : clothes){
+            map.putIfAbsent(s[1], 0);
+            map.put(s[1], map.get(s[1]) + 1);
+        }
+        
+        for(String s : map.keySet()){
+            answer *= map.get(s) + 1;
+        }
+        
+        return answer - 1;
+    }
+}
