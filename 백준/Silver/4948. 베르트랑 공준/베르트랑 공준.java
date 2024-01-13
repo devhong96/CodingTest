2bd879a8b06caf7cc383fef95ac7db86b4e1a55e
Seho0218 <dev.hsayho@gmail.com>
2024-01-13 18:59:42 +0900

    [Silver II] Title: 베르트랑 공준, Time: 176 ms, Memory: 14820 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,30 +1,44 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        while(true){
-            int num = Integer.parseInt(br.readLine());
-            if(num == 0) break;
-            int answer = 0;
-            for(int i = num + 1; i <= 2 * num; i++){
-                if(isPrime(i)) answer++;
-            }
-            System.out.println(answer);
-        }
-    }
-
-    static boolean isPrime(int n){
-        if(n == 1 || n == 0) return false;
-        for(int i = 2; i * i <= n; i++){
-            if(n % i == 0) return false;
-        }
-        return true;
-    }
+	/*
+	n > 123456 이므로 2n 은 최대 246912 이다.
+	0 부터 시작하므로 246912 + 1
+	*/
+	public static boolean[] prime = new boolean[246913];
+    
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		get_prime();	// 소수를 얻는 메소드 실행
+		
+		while(true) {
+			int n = Integer.parseInt(br.readLine());
+			if(n == 0) break;	// n 이 0 일경우 종료
+            
+			int count = 0;	// 소수 개수를 셀 변수
+            
+			for(int i = n + 1; i <= 2 * n; i++) {
+				if(!prime[i]) count++;
+			}
+			System.out.println(count);
+		}		
+	}
+	
+	// 소수를 얻는 메소드
+	public static void get_prime() {
+		// 0 과 1 은 소수가 아니므로 ture
+		prime[0] = prime[1] = true;
+		
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if(prime[i]) continue;
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+	}
+ 
 }
