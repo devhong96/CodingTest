926cc5858cd63e1c0b6cc47199efdd7ea4bf29ca
Seho0218 <dev.hsayho@gmail.com>
2024-01-18 03:28:58 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 756 ms, Memory: 40660 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,36 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static boolean isPrime(int num){
-        if(num == 1) return false;
-        for(int i = 2; i * i <= num; i++){
-            if(num % i == 0){
-                return false;
-            }
-        }
-        return true;
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int[] arr = new int[a];
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        HashSet<Integer> set = new HashSet<Integer>();
-        int N = scanner.nextInt();
-
-        for(int i = 2; i < 1000000; i++){
-            if(isPrime(i)) set.add(i);
+        for (int i = 0; i < a; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        ArrayList<Integer> arr = new ArrayList<Integer>(set);
-        Collections.sort(arr);
-        for(int i = 0; i < N; i++){
-            int num = scanner.nextInt();
-            int cnt = 0;
-            for(int j = 0; arr.get(j) < num ; j++){
-                if(num - arr.get(j) < arr.get(j)) break;
-                if(set.contains(num - arr.get(j))) {
-                    cnt += 1;
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int j = 2; j < 1000001; j++){
+            if(isPrime(j)) set.add(j);
+        }
+        ArrayList<Integer> primeList = new ArrayList<>(set);
+        Collections.sort(primeList);
+        for(int num = 0; num < a; num++){
+            int answer = 0;
+            for(int k = 0; primeList.get(k) < arr[num]; k++){
+                if(arr[num] - primeList.get(k) < primeList.get(k)) break;
+                if(set.contains(arr[num] - primeList.get(k))) {
+                    answer+=1;
                 }
             }
-            System.out.println(cnt);
+            System.out.println(answer);
         }
-        scanner.close();
+    }
+
+    static boolean isPrime(int n){
+        if(n == 0 || n == 1) return false;
+        for(int i = 2; i * i <= n; i++){
+            if(n % i == 0) return false;
+        }
+        return true;
     }
 }
