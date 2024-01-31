ec9584d01f5437ef4e4d42d09251cba386def34e
Seho0218 <dev.hsayho@gmail.com>
2024-01-31 14:43:41 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14700 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,53 +1,76 @@
-import java.io.*;
+import javax.swing.plaf.nimbus.State;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static boolean[][] visited;
-    static int[][] A;
-    static int X, Y;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        X = Integer.parseInt(st.nextToken());
-        Y = Integer.parseInt(st.nextToken());
-        A = new int[X][Y];
-        visited = new boolean[X][Y];
+        StringBuilder sb = new StringBuilder();
+        Solution solution = new Solution();
+        StringTokenizer st;
 
-        for (int i = 0; i < X; i++) {
-            st = new StringTokenizer(br.readLine());
-            String line = st.nextToken();
-            for (int j = 0; j < Y; j++) {
-                A[i][j] = Integer.parseInt(line.substring(j, j + 1));
+        st = new StringTokenizer(br.readLine());
+
+        int line = Integer.parseInt(st.nextToken());
+        int length = Integer.parseInt(st.nextToken());
+
+        int[][] maps = new int[line][length];
+
+        for(int i = 0; i < line; i++){
+            String s = br.readLine();
+            for(int j = 0; j < length; j++){
+                maps[i][j] = s.charAt(j) - '0';
             }
         }
+        System.out.print(solution.solution(maps));
+    }
+}
 
-        BFS(0, 0);
-        System.out.println(A[X - 1][Y - 1]);
+class Solution {
+    private static class State{
+        public final int x;
+        public final int y;
+        public final int step;
+
+        private State(int x, int y, int step){
+            this.x = x;
+            this.y = y;
+            this.step = step;
+        }
     }
 
-    public static void BFS(int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{i, j});
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-//            System.out.println("now = " + Arrays.toString(now));
-            visited[i][j] = true;
-            for (int k = 0; k < 4; k++) {
-                int x1 = now[0] + dx[k];
-//                System.out.print("x = " + x);
-                int y1 = now[1] + dy[k];
-//                System.out.println(" y = " + y);
-                if (x1 >= 0 && y1 >= 0 && x1 < X && y1 < Y) {
-                    if (A[x1][y1] != 0 && !visited[x1][y1]) {
-                        visited[x1][y1] = true;
-                        A[x1][y1] = A[now[0]][now[1]] + 1;
-                        queue.add(new int[]{x1, y1});
-                    }
-                }
+    private static final int[] dx = {0, 1, 0, -1};
+    private static final int[] dy = {-1, 0, 1, 0};
+
+    public int solution(int[][] maps) {
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+
+        Queue<State> queue = new LinkedList<>();
+        queue.add(new State(0, 0, 1));
+        visited[0][0] = true;
+
+        while(!queue.isEmpty()){
+            State state = queue.poll();
+
+            if(state.y == maps.length - 1 && state.x == maps[state.y].length - 1){
+                return state.step;
+            }
+
+            for(int d = 0; d < 4; d++){
+                int nx = state.x + dx[d];
+                int ny = state.y + dy[d];
+
+                if(nx < 0 || ny < 0 || nx >= maps[0].length || ny >= maps.length) continue;
+
+                if(maps[ny][nx] != 1) continue;
+                if(visited[ny][nx]) continue;
+
+                visited[ny][nx] = true;
+                queue.add(new State(nx, ny, state.step + 1));
             }
         }
+        return -1;
     }
 }
