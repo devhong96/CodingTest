26f044e0d908a2e6b1eaf5759df739f30a37239c
Seho0218 <dev.hsayho@gmail.com>
2024-03-05 15:25:48 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        int[] answer = {0, 0};
+
+        for(int i = 0; i < words.length; i++){
+            if(i != 0){
+                String s1 = words[i - 1];
+                String s2 = words[i];
+                
+                char last = s1.charAt(s1.length() - 1);
+                char first = s2.charAt(0);
+                
+                if(map.containsKey(s2) || last != first){
+                    answer[0] = (i % n) + 1;
+                    answer[1] = (i / n) + 1;
+                    return answer;
+                }
+            }
+            map.putIfAbsent(words[i], 1);
+        }
+        return answer;
+    }
+}
