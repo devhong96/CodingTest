b5db8415c6e088e72d8871d35e45d636d35fd33e
Seho0218 <dev.hsayho@gmail.com>
2024-03-15 01:38:00 +0900

    [Silver II] Title: 이항 계수 2, Time: 140 ms, Memory: 18260 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Solution solution = new Solution();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        solution.solution(n, k);
+    }
+}
+
+class Solution {
+    public void solution(int n, int k) {
+        int[][] dy = new int[n + 1][n + 1];
+        for(int i = 0; i <= n; i++){
+            dy[i][0] = 1;
+            dy[i][1] = i;
+            dy[i][i] = 1;
+        }
+
+        for(int i = 2; i <= n; i++){
+            for(int j = 1; j < i; j++){
+                dy[i][j] = dy[i - 1][j - 1] + dy[i - 1][j];
+                dy[i][j] = dy[i][j] % 10007;
+            }
+        }
+        System.out.println(dy[n][k]);
+    }
+}
