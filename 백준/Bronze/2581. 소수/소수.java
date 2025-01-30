a1fad74281eacc3b5a9cc29866ad7b32e04a706a
devhong96 <dev.hong96@gmail.com>
2025-01-30 23:58:13 +0900

    [Bronze II] Title: 소수, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,36 +1,32 @@
 import java.io.*;
+import java.util.*;
 
 class Main{
     public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
+        int answer = 0, min = -1;
+        int from = Integer.parseInt(br.readLine());
+        int to = Integer.parseInt(br.readLine());
 
-        int sum = 0, min = 0;;
-        for(int i = a; i <= b; i++){
-            if(isPrime(i)){
-                sum += i;
+        for (int i = from; i <= to; i++) {
+            if (isPrime(i)) {
+                answer += i;
+                if(min == -1) min = i;
             }
         }
-
-        for(int i = a; i <= b; i++){
-            if(isPrime(i)) {
-                min = i;
-                break;
-            }
-        }
-        
-        if(sum != 0) {
-            System.out.println(sum);
-            System.out.print(min);
-        }else{
+        if (answer == 0) {
             System.out.println(-1);
+        } else {
+            System.out.println(answer);
+            System.out.println(min);
         }
     }
+
+
     public static boolean isPrime(int n){
-        if(n == 1 || n == 0) return false;
-        for(int i = 2; i * i <= n; i++){
+        if(n == 0 || n == 1) return false;
+        for (int i = 2; i * i <= n; i++) {
             if(n % i == 0) return false;
         }
         return true;
