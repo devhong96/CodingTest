a7b1a5c93986b036321bdddda60dd071b4096e3f
devhong96 <dev.hong96@gmail.com>
2025-04-28 06:18:54 +0900

    [Gold II] Title: 보석 도둑, Time: 1360 ms, Memory: 144620 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,47 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
- 
+
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
- 
-        int n = scan.nextInt();
-        int k = scan.nextInt();
-        
-        ArrayList<Gem> list = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            int m = scan.nextInt();
-            int v = scan.nextInt();
-            list.add(new Gem(m, v));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Solution solution = new Solution();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int [][] jewel = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            jewel[i][0] = Integer.parseInt(st.nextToken());
+            jewel[i][1] = Integer.parseInt(st.nextToken());
         }
-        Collections.sort(list, (o1, o2) -> o1.m - o2.m); //무게순 정렬
-        
-        int[] weight = new int[k];
-        for(int i = 0; i < k; i++) {
-            weight[i] = scan.nextInt();
+
+        ArrayList<Integer> back = new ArrayList<>();
+
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            back.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(weight);
-        
-        long total = 0;
-        int listIdx = 0;
-        PriorityQueue<Gem> pq = new PriorityQueue<>((o1, o2) -> o2.v - o1.v); //가격순 정렬
-        for(int i = 0; i < k; i++) {
-            while(listIdx < n && list.get(listIdx).m <= weight[i]) {
-                Gem current = list.get(listIdx++);
-                pq.add(new Gem(current.m, current.v));
-            }
-            if(!pq.isEmpty()) total += pq.poll().v;
-        }
-        System.out.println(total);
+
+        System.out.println(solution.solution(jewel, back));
     }
-    
-    public static class Gem {
-        int m;
-        int v;
-        
-        public Gem(int m, int v) {
+}
+
+class Solution {
+    static class Jewel {
+        final int m;
+        final int v;
+
+        Jewel(int m, int v) {
             this.m = m;
             this.v = v;
         }
+    }
+    public long solution(int[][] jewels, ArrayList<Integer> backList) {
+
+        ArrayList<Jewel> jewelList = new ArrayList<>();
+
+        for (int[] jewel : jewels) {
+            int m = jewel[0];
+            int v = jewel[1];
+
+            jewelList.add(new Jewel(m, v));
+        }
+
+        jewelList.sort(Comparator.comparingInt(p -> p.m));
+
+        Collections.sort(backList);
+        PriorityQueue<Jewel> pq = new PriorityQueue<>((p1, p2) -> p2.v - p1.v);
+
+        long answer = 0;
+        int index = 0;
+
+        for (Integer back : backList) {
+            while (index < jewelList.size()
+                    && jewelList.get(index).m <= back) {
+                Jewel jewel = jewelList.get(index++);
+                pq.offer(jewel);
+            }
+
+           if(!pq.isEmpty()) answer += pq.poll().v;
+
+        }
+
+        return answer;
     }
 }
