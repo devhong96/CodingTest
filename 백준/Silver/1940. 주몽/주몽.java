b8ee919f156ac34b2215bd2d3222d95642f537b6
devhong96 <dev.hong96@gmail.com>
2025-06-27 12:23:08 +0900

    [Silver IV] Title: 주몽, Time: 176 ms, Memory: 16428 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -15,32 +15,35 @@ public class Main {
         int target = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
+
         for(int i = 0; i < arr.length; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        solution.solution(arr, target);
+        System.out.println(solution.solution(arr, target));
     }
 }
 
 class Solution {
-    public void solution(int[] arr, int target) {
+    public int solution(int[] arr, int target) {
 
         int lt = 0, rt = arr.length - 1, answer = 0;
 
         Arrays.sort(arr);
-        
-        while(lt < rt){
-            if(arr[lt] + arr[rt] > target){
-                rt--;
-            }else if(arr[lt] + arr[rt] < target){
-                lt++;
-            }else{
+
+        while(lt < rt) {
+            int sum = arr[lt] + arr[rt];
+            if (sum == target) {
                 answer++;
                 lt++;
                 rt--;
+            } else if (sum < target) {
+                lt++;
+            } else {
+                rt--;
             }
         }
-        System.out.print(answer);
+
+        return answer;
     }
 }
