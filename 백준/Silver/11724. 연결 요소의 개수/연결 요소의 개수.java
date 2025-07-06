3b5458001634c0039c181804b07f3f525255b520
devhong96 <dev.hong96@gmail.com>
2025-07-07 07:53:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 504 ms, Memory: 131084 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,50 +1,74 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.*;
+
 public class Main {
-  static ArrayList<Integer>[] A;
-  static boolean visited[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        Solution solution = new Solution();
+        StringTokenizer st;
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
 
-        A = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[M][2];
 
-        for (int i = 1; i < n + 1; i++) {
-            A[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
 
-            A[s].add(e);
-            A[e].add(s);
+            arr[i][0] = Integer.parseInt(st.nextToken()) - 1;
+            arr[i][1] = Integer.parseInt(st.nextToken()) - 1;
         }
+
+        System.out.println(solution.solution(N, arr));
+
+    }
+}
+
+class Solution {
+
+    int[][] graph;
+    boolean[] visited;
+    int node;
+    public int solution(int N, int[][] arr) {
 
         int count = 0;
-        for (int i = 1; i < n+1; i++) {
-            if(!visited[i]){
+
+        graph = new int[N][N];
+        visited = new boolean[N];
+        node = N;
+
+        for (int[] ar : arr) {
+            graph[ar[0]][ar[1]] = 1;
+            graph[ar[1]][ar[0]] = 1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if(!visited[i]) {
                 count++;
-                DFS(i);
+                bfs(i);
             }
         }
 
-        System.out.println(count);
+        return count;
     }
 
-    private static void DFS(int i) {
-        if(visited[i]){
-            return;
-        }
-        visited[i] = true;
-        for(int k : A[i]){
-            if(!visited[k]){
-                DFS(k);
+    private void bfs(int start) {
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            Integer poll = queue.poll();
+
+            for (int n = 0; n < node; n++) {
+                if(graph[poll][n] == 1 && !visited[n]){
+                    queue.offer(n);
+                    visited[n] = true;
+                }
             }
         }
     }
