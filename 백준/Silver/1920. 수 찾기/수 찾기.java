da2067f43f9c63e0620e6493de7b69e01e2fdbd0
devhong96 <dev.hong96@gmail.com>
2025-12-18 19:20:00 +0900

    [Silver IV] Title: 수 찾기, Time: 580 ms, Memory: 44564 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,63 +1,62 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N = 0;
-	static int[] a;
-	
-	static boolean bs(int k) {
-		
-	    // l, r, m은 index 일뿐 실제 값을 a에서 찾아 비교해야 한다.
-		int l = 0;
-		int r = N - 1;
-		
-		if (a[l] > k || a[r] < k) {
-			return false;
-		}
-		
-		while (l <= r) {
-	        int m = (l + r) / 2;
-	        
-	        if (a[m] == k)
-	        {
-	            return true;
-	        }
-	        else if (a[m] > k)
-	        {
-	            r = m - 1;
-	        }
-	        else
-	        {
-	            l = m + 1;
-	        }
-		}
-		
-		return false;
-		
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		a = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			a[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(a);
-		
-		int M = Integer.parseInt(br.readLine());
-		int[] b = new int[M];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
-			b[i] = Integer.parseInt(st.nextToken());
-			if (bs(b[i])) {
-				System.out.println("1");
-			} else {
-				System.out.println("0");				
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Solution solution = new Solution();
+        StringTokenizer st;
+
+        int num = Integer.parseInt(br.readLine());
+        long[] arr = new long[num];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < num; i++){
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        int num2 = Integer.parseInt(br.readLine());
+        long[] arr2 = new long[num2];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < num2; i++){
+            arr2[i] = Long.parseLong(st.nextToken());
+        }
+
+        System.out.println(solution.solution(arr, arr2));
+    }
+}
+
+class Solution {
+    public String solution(long[] original, long[] target) {
+
+        StringBuilder sb = new StringBuilder();
+
+        Arrays.sort(original);
+
+        for (int i = 0; i < target.length; i++) {
+            int lt = 0, rt = original.length - 1;
+            int result = 0;
+
+            while (lt <= rt) {
+                int mid = (lt + rt) / 2;
+
+                if (original[mid] == target[i]) {
+                    result = 1;
+                    break;
+                }
+
+                if (original[mid] < target[i]) lt = mid + 1;
+                else rt = mid - 1;
+            }
+
+            sb.append(result).append('\n');
+        }
+
+        return sb.toString();
+    }
 }
